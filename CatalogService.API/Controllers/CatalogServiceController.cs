d7341c7cba0eac69fe237463baeb5917b5aa68dc
Den V. Lebedev <dvl@inbox.ru>
2023-03-23 13:51:18 +0300

    Changed controller logic

--- CatalogService.API/Controllers/CatalogServiceController.cs
+++ CatalogService.API/Controllers/CatalogServiceController.cs
@@ -1,3 +1,5 @@
+using CatalogService.Application.Interfaces;
+using CatalogService.Core;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CatalogService.API.Controllers
@@ -6,6 +8,21 @@ namespace CatalogService.API.Controllers
     [Route("[controller]")]
     public class CatalogServiceController : ControllerBase
     {
-        
+        private readonly ICatalogService _service;
+        private readonly ILogger<CatalogServiceController> _logger;
+
+        public CatalogServiceController(ILogger<CatalogServiceController> logger, ICatalogService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> Get(int categoryId)
+        {
+            var result = _service.GetItemsInCategory(categoryId);
+
+            return result;
+        }
     }
 }
