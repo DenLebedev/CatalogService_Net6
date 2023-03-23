a866544706a7677ea044e53ba09793e63c80f010
Den V. Lebedev <dvl@inbox.ru>
2023-03-23 13:00:12 +0300

    Add project files.

---
+++ CatalogService.API/Controllers/CatalogServiceController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogServiceController : ControllerBase
+    {
+        
+    }
+}
